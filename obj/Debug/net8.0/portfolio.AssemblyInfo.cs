19c9d097b9643cf136bc51b975d5c149e52c8611
yahir91 <yahir.cardona@gmail.com>
2024-04-24 11:28:29 -0600

    feat: created contact model

--- obj/Debug/net8.0/portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebf18dbbbad330358a0fe5f83a50e7f5f492f4c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b2904ea11ec877f4ebf31ae99a16b6b0ffec3a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
