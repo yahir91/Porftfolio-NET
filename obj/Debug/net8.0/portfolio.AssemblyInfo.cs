ebf18dbbbad330358a0fe5f83a50e7f5f492f4c8
yahir91 <yahir.cardona@gmail.com>
2024-04-24 11:12:42 -0600

    feat: created skills view

--- obj/Debug/net8.0/portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe74d6fccaf5cbd59b1f708cc657f9a96e14968f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+508c682e8270b43ce08b66429a3cfe7477f836a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
